6a0f33ae6c410791660ef93d5917069ab7fa2121
ted <ted1998917@gmail.com>
2024-09-21 10:22:26 +0930

    server can accept multiple socket connection and print

--- src/AggregationServer.java
+++ src/AggregationServer.java
@@ -1,8 +1,73 @@
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class AggregationServer{
-    public static void main(String[] args)  {
+    private final int port = 4567;
+    private Socket socket   = null;
+    private ServerSocket server   = null;
+    public AggregationServer()
+    {
+        try {
+            server = new ServerSocket(port);
+            System.out.println("Server started");
+            System.out.println("Waiting for a client ...");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
 
+        }
+        while (true) {
+
+            try {
+                socket = server.accept();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            // new thread for a client
+            new SocketThread(socket).start();
+        }
+
+    }
+    public static void main(String[] args)  {
+        AggregationServer aggregationServer = new AggregationServer();
+    }
+
+    class SocketThread extends Thread{
+        private Socket socket;
+        public SocketThread(Socket clientSocket) {
+            this.socket = clientSocket;
+        }
+        public void run() {
+            InputStream inp = null;
+            BufferedReader brinp = null;
+            DataOutputStream out = null;
+
+            try {
+                inp = socket.getInputStream();
+                brinp = new BufferedReader(new InputStreamReader(inp));
+                out = new DataOutputStream(socket.getOutputStream());
+            } catch (IOException e) {
+                return;
+            }
+            String line;
+            while (true) {
+                try {
+                    line = brinp.readLine();
+                    if ((line == null) || line.equalsIgnoreCase("done")) {
+                        socket.close();
+                        return;
+                    } else {
+                        System.out.println(line);
+                        out.writeBytes(line + "\n\r");
+                        out.flush();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return;
+                }
+            }
+        }
     }
 }
--- src/ContentServer.java
+++ src/ContentServer.java
@@ -1,6 +1,51 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class ContentServer{
-    public static void main(String[] args){
-
-
+    private String address = "127.0.0.1";
+    private int port = 4567;
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream out = null;
+    public ContentServer(){
+        try {
+            var socket = new Socket(address, port);
+            System.out.println("Connected");
+            input = new DataInputStream(System.in);
+            out = new DataOutputStream(
+                    socket.getOutputStream());
+        }
+        catch (UnknownHostException u) {
+            System.out.println(u);
+            return;
+        }
+        catch (IOException i) {
+            System.out.println(i);
+            return;
+        }
+        String line = "";
+        while (!line.equals("done")) {
+            try {
+                line = input.readLine();
+                out.writeBytes(line+"\n");
+            }
+            catch (IOException i) {
+                System.out.println(i);
+            }
+        }
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        }
+        catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+        ContentServer server = new ContentServer();
     }
 }
